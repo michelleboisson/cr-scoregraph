3e880af9284a525717bc1d0858df281340d25a94
Michelle Boisson <michelle.boisson@gmail.com>
2017-02-28 11:09:44 -0500

    not sure

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 A data-driven prototype for visualizing the range of tested refrigerators and their scores
 
 https://cr-overallscoregraph.herokuapp.com/ 
+
+## How to Run the App on your Local Machine
+- install meteor https://www.meteor.com/install
+- using Terminal command line, "cd" into this directory
+- "meteor" to run the app
+- open a new browser window, navigate to http://localhost:3000
+
+## How to edit the product category
+The app is making an API call to get all products in a category, and copies the data to a database.
+The data displayed in the app is from this database.
+
+STEP 1: Change API_CATEGORY in glb.js
+STEP 2: While the app in running in your browser, open the console and run " Meteor.call('refreshProducts') "
+
--- server/main.js
+++ server/main.js
@@ -21,6 +21,8 @@ Meteor.startup(function(){
         for (var i=0; i<myarray.length; i++){
           var item = myarray[i];
           item.index = i;
+          item.productType = API_CATEGORY; // use this if we want to refactor to be able to change products on the frontend 
+            // would need to change filters to include productType filter always
 
           //ProductDatabase.insert(item);
         }
@@ -29,12 +31,40 @@ Meteor.startup(function(){
     
     return Meteor.methods({
 
-      removeAllProducts: function() {
-        console.log("removing all products");
-        return ProductDatabase.remove({});
+      refreshProducts: function() {
+        console.log("refreshing all products");
+        return ProductDatabase.remove({}, function(){
+          //load data from API: toasters
+          
+          var apiLink = "http://api.consumerreports.org/v0.1/products.(category.name="+API_CATEGORY+").json?api_key="+API_KEY+"&numResults=5000";
+          myarray = HTTP.get(apiLink).data;
+  
+
+          if (ProductDatabase.find({}).count() <= 0){
+            for (var i=0; i<myarray.length; i++){
+              var item = myarray[i];
+              item.index = i;
+              item.productType = API_CATEGORY; // use this if we want to refactor to be able to change products on the frontend 
+              // would need to change filters to include productType filter always
+
+              ProductDatabase.insert(item);
+            }
+          }
+
+        });
       }
 
     });
 
   });
 
+/* TO DO
+
+- create page level filter for changing products on the front end
+    - import more product cats
+    - include new productType filter in all filter
+    - include dropdown at top of page to switch the content
+    - edit title in page??
+- 
+
+*/
